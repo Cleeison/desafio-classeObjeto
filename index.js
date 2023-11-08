bfc448fdd215051e816a61d0defab1baa037866f
Cleison <cleison.ds99@gmail.com>
2023-11-08 17:55:52 -0300

    subindo desafio

---
+++ index.js
@@ -0,0 +1,54 @@
+/*
+    # 3️⃣ Escrevendo as classes de um Jogo
+
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+- Funções
+- Classes e Objetos
+
+## Objetivo:
+
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+- nome
+- idade
+- tipo (ex: guerreiro, mago, monge, ninja )
+
+além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+se mago -> no ataque exibir (usou magia)
+se guerreiro -> no ataque exibir (usou espada)
+se monge -> no ataque exibir (usou artes marciais)
+se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}"
+  ex: mago atacou usando magia
+  guerreiro atacou usando espada
+*/
+
+class hero {
+    constructor(name,age,type,hit){
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.hit = hit;
+    }
+    attack(){
+        console.log(`O ${this.type} ${this.name} atacou usando ${this.hit}`);
+    }
+}
+
+let warrior = new hero("Kratos",46,"guerreiro","espada");
+warrior.attack();
